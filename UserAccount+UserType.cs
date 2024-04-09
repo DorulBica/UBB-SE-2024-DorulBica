74c21cbceb3dffa7f4b91f026a852793931e1ae2
BicaDoru <115446962+BicaDoru@users.noreply.github.com>
2024-04-09 14:57:27 +0300

    Update UserAccount+UserType.cs

--- UserAccount+UserType.cs
+++ UserAccount+UserType.cs
@@ -1,116 +1,118 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 
-public class UserType
+namespace UserManagement
 {
-    // Field
-    private string type;
-
-    // Property
-    public string Type
+    public class UserType
     {
-        get { return type; }
-        set { type = value; }
-    }
+        // Field
+        private string type;
 
-    // Constructor
-    public UserType(string type)
-    {
-        Type = type;
-    }
-}
-
-
-public class UserAccount
-{
-    // Fields
-    private string username;
-    private string password;
-    private string emailAddress;
-    private string socialMediaAccount;
-    private bool banned;
-    private UserType userType;
-
-    // Properties
-    public string Username
-    {
-        get { return username; }
-        set { username = value; }
-    }
-
-    public string Password
-    {
-        get { return password; }
-        set { password = value; }
-    }
-
-    public string EmailAddress
-    {
-        get { return emailAddress; }
-        set { emailAddress = value; }
-    }
-
-    public string SocialMediaAccount
-    {
-        get { return socialMediaAccount; }
-        set { socialMediaAccount = value; }
-    }
-
-    public bool Banned
-    {
-        get { return banned; }
-        set { banned = value; }
-    }
-
-    public UserType UserType
-    {
-        get { return userType; }
-        set { userType = value; }
-    }
-
-    // Constructor
-    public UserAccount(string username, string password, string emailAddress, string socialMediaAccount, bool banned, UserType UserType)
-    {
-        Username = username;
-        Password = password;
-        EmailAddress = emailAddress;
-        SocialMediaAccount = socialMediaAccount;
-        Banned = banned;
-        userType = UserType;
-    }
-
-    public static UserAccount RegisterAccount(string username, string password, string emailAddress, string socialMediaAccount, UserType userType, List<UserAccount> existingAccounts)
-    {
-        // Check if the username is unique
-        if (IsUsernameUnique(username, existingAccounts))
+        // Property
+        public string Type
         {
-            // Creating a new user account
-            UserAccount newUser = new UserAccount(username, password, emailAddress, socialMediaAccount, false, userType);
-            Console.WriteLine("Account registered successfully!");
-            return newUser;
+            get { return type; }
+            set { type = value; }
         }
-        else
+
+        // Constructor
+        public UserType(string type)
         {
-            Console.WriteLine("Username already exists. Please choose a different username.");
-            return null;
+            Type = type;
         }
     }
 
-    // Helper function to check if the username is unique
-    private static bool IsUsernameUnique(string username, List<UserAccount> existingAccounts)
+    public class UserAccount
     {
-        foreach (UserAccount account in existingAccounts)
+        // Fields
+        private string username;
+        private string password;
+        private string emailAddress;
+        private string socialMediaAccount;
+        private bool banned;
+        private UserType userType;
+
+        // Properties
+        public string Username
         {
-            if (account.Username == username)
+            get { return username; }
+            set { username = value; }
+        }
+
+        public string Password
+        {
+            get { return password; }
+            set { password = value; }
+        }
+
+        public string EmailAddress
+        {
+            get { return emailAddress; }
+            set { emailAddress = value; }
+        }
+
+        public string SocialMediaAccount
+        {
+            get { return socialMediaAccount; }
+            set { socialMediaAccount = value; }
+        }
+
+        public bool Banned
+        {
+            get { return banned; }
+            set { banned = value; }
+        }
+
+        public UserType UserType
+        {
+            get { return userType; }
+            set { userType = value; }
+        }
+
+        // Constructor
+        public UserAccount(string username, string password, string emailAddress, string socialMediaAccount, bool banned, UserType userType)
+        {
+            Username = username;
+            Password = password;
+            EmailAddress = emailAddress;
+            SocialMediaAccount = socialMediaAccount;
+            Banned = banned;
+            UserType = userType;
+        }
+
+        public static UserAccount RegisterAccount(string username, string password, string emailAddress, string socialMediaAccount, UserType userType, List<UserAccount> existingAccounts)
+        {
+            // Check if the username is unique
+            if (IsUsernameUnique(username, existingAccounts))
             {
-                return false;
+                // Creating a new user account
+                UserAccount newUser = new UserAccount(username, password, emailAddress, socialMediaAccount, false, userType);
+                Console.WriteLine("Account registered successfully!");
+                return newUser;
+            }
+            else
+            {
+                Console.WriteLine("Username already exists. Please choose a different username.");
+                return null;
             }
         }
-        return true;
-    }
 
-    public override string ToString()
-    {
-        return $"Username: {Username}, Email Address: {EmailAddress}, Social Media Account: {SocialMediaAccount}, Banned: {Banned}, UserType: {UserType.Type}";
+        // Helper function to check if the username is unique
+        private static bool IsUsernameUnique(string username, List<UserAccount> existingAccounts)
+        {
+            foreach (UserAccount account in existingAccounts)
+            {
+                if (account.Username == username)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public override string ToString()
+        {
+            return $"Username: {Username}, Email Address: {EmailAddress}, Social Media Account: {SocialMediaAccount}, Banned: {Banned}, UserType: {UserType.Type}";
+        }
     }
 }
