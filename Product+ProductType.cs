23450d7b80ecc584f4f98676f78b91f47056fecb
BicaDoru <115446962+BicaDoru@users.noreply.github.com>
2024-04-09 14:02:45 +0300

    Add files via upload
    classes Product, ProductType, UserAccount, UserType

---
+++ Product+ProductType.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class ProductType
+{
+    // Field
+    private string type;
+
+    // Property
+    public string Type
+    {
+        get { return type; }
+        set { type = value; }
+    }
+
+    // Constructor
+    public ProductType(string type)
+    {
+        Type = type;
+    }
+}
+
+public class Product
+{
+    // Fields
+    private int drugID;
+    private string drugName;
+    private string description;
+    private double price;
+    private ProductType category;
+    private int quantity;
+
+    // Properties
+    public int DrugID
+    {
+        get { return drugID; }
+        set { drugID = value; }
+    }
+
+    public string DrugName
+    {
+        get { return drugName; }
+        set { drugName = value; }
+    }
+
+    public string Description
+    {
+        get { return description; }
+        set { description = value; }
+    }
+
+    public double Price
+    {
+        get { return price; }
+        set { price = value; }
+    }
+
+    public ProductType Category
+    {
+        get { return category; }
+        set { category = value; }
+    }
+
+    public int Quantity
+    {
+        get { return quantity; }
+        set { quantity = value; }
+    }
+
+    // Constructor
+    public Product(int drugID, string drugName, string description, double price, ProductType category, int quantity)
+    {
+        DrugID = drugID;
+        DrugName = drugName;
+        Description = description;
+        Price = price;
+        Category = category;
+        Quantity = quantity;
+    }
+}
---
+++ UserAccount+UserType.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class UserType
+{
+    // Field
+    private string type;
+
+    // Property
+    public string Type
+    {
+        get { return type; }
+        set { type = value; }
+    }
+
+    // Constructor
+    public UserType(string type)
+    {
+        Type = type;
+    }
+}
+
+
+public class UserAccount
+{
+    // Fields
+    private string username;
+    private string password;
+    private string emailAddress;
+    private string socialMediaAccount;
+    private bool banned;
+    private UserType userType;
+
+    // Properties
+    public string Username
+    {
+        get { return username; }
+        set { username = value; }
+    }
+
+    public string Password
+    {
+        get { return password; }
+        set { password = value; }
+    }
+
+    public string EmailAddress
+    {
+        get { return emailAddress; }
+        set { emailAddress = value; }
+    }
+
+    public string SocialMediaAccount
+    {
+        get { return socialMediaAccount; }
+        set { socialMediaAccount = value; }
+    }
+
+    public bool Banned
+    {
+        get { return banned; }
+        set { banned = value; }
+    }
+
+    public UserType UserType
+    {
+        get { return userType; }
+        set { userType = value; }
+    }
+
+    // Constructor
+    public UserAccount(string username, string password, string emailAddress, string socialMediaAccount, bool banned, UserType UserType)
+    {
+        Username = username;
+        Password = password;
+        EmailAddress = emailAddress;
+        SocialMediaAccount = socialMediaAccount;
+        Banned = banned;
+        userType = UserType;
+    }
+
+    public static UserAccount RegisterAccount(string username, string password, string emailAddress, string socialMediaAccount, UserType userType, List<UserAccount> existingAccounts)
+    {
+        // Check if the username is unique
+        if (IsUsernameUnique(username, existingAccounts))
+        {
+            // Creating a new user account
+            UserAccount newUser = new UserAccount(username, password, emailAddress, socialMediaAccount, false, userType);
+            Console.WriteLine("Account registered successfully!");
+            return newUser;
+        }
+        else
+        {
+            Console.WriteLine("Username already exists. Please choose a different username.");
+            return null;
+        }
+    }
+
+    // Helper function to check if the username is unique
+    private static bool IsUsernameUnique(string username, List<UserAccount> existingAccounts)
+    {
+        foreach (UserAccount account in existingAccounts)
+        {
+            if (account.Username == username)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
