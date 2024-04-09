1ec9e41f13743193e9a22d45f31bf6467f89557f
bianca-bobes <bianca.bobes@stud.ubbcluj.ro>
2024-04-09 19:49:06 +0300

    Cart

---
+++ Cart.xaml.cs
@@ -0,0 +1,27 @@
+﻿using ProductManagement;
+using System.Collections.Generic;
+using System.Windows;
+
+namespace InjectNow
+{
+    public partial class Cart : Window
+    {
+        // Define products as a public property
+        public List<Product> Products { get; set; }
+
+        public Cart()
+        {
+            InitializeComponent();
+
+            // Initialize the products list and add products
+            Products = new List<Product>
+            {
+                new Product(1, "Cocaine", "mental arousal", 5.99, new ProductType("CNS Stimulants"), 10),
+                new Product(2, "Marijuana", "relaxer", 2.49, new ProductType("Cannabis"), 20)
+            };
+
+            // Bind the list of products to the DataContext of the window
+            DataContext = this;
+        }
+    }
+}
