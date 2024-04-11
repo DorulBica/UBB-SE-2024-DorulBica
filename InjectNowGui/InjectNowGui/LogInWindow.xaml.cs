fa3675534ccc9400d37b1bfc8b7d1b75405d3150
Badea Vlad-Stefan <115445900+BadeaVladStefan@users.noreply.github.com>
2024-04-11 13:34:10 +0300

    Update LogInWindow.xaml.cs

--- InjectNowGui/InjectNowGui/LogInWindow.xaml.cs
+++ InjectNowGui/InjectNowGui/LogInWindow.xaml.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,6 +22,27 @@ namespace InjectNowGui
         public LogInWindow()
         {
             InitializeComponent();
+        }
+
+        private void SignUpButton_Click(object sender, RoutedEventArgs e)
+        {
+            SignUpPage signUpPage = new SignUpPage();
+            signUpPage.Show();
+            this.Close();
+        }
+
+        private void SignInButton_Click(object sender, RoutedEventArgs e)
+        {
+            string username = UsernameTextBox.Text;
+            string password = PasswordTextBox.Text;
+            string email = EmailTextBox.Text;
+
+            if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password) || string.IsNullOrEmpty(email))
+            {
+                MessageBox.Show("Please fill in all fields.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
         }
     }
 }
